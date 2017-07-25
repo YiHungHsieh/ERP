7ee57e0501461e05d67e5b338429747badc87189
David <davidhsieh7988@gmail.com>
2017-07-25 15:20:29 +0800

    first update

---
+++ Lctech/leave.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>ERP</title>
+<link type="text/css" rel="stylesheet"  href="ERP_system.css">
+<link type="text/css" rel="stylesheet"  href="form.css">
+
+</head>
+<body>
+
+<div class="page">
+	<div id="nav-bar">
+		<ul>
+		<li><img src="http://www.lctech.com.tw/wp-content/uploads/bfi_thumb/未命名-1-拷貝-31w65e1o8bhj94p9q5patm.png"></li>
+		<li class="item">線上打卡</li>
+		<li class="item">表單申請</li>
+		<li class="item">公告事項</li>
+		</ul>
+	</div>	
+	<div class="sidebar">
+		<div class="profile">
+			<p>My Profile</p>
+			<p><img src="http://static.commentcamarche.net/www.commentcamarche.net/pictures/nLJSfM5U6lLTQbiUtwXmdiUYajn9aJAyvjxpfXbOkdUUMQWnZ3OdtFhB4zwEAhAW-efa5cf51c0711fafc61e73f90e05bc12-s-.png" width="106" height="106"></p>
+			<p><hr></p>
+			<p>name</p>
+			<p>region</p>
+			<p>birth</p>
+		</div>
+		<div class="form-apply">
+			<p>請假申請</p>
+			<p>出差申請</p>
+			<p>差旅費申請</p>
+		</div>
+	</div>
+	<div class="content">
+		<div class="leave"><h3>請假申請</h3></div>
+		<form>
+		<p>表單編號：<input type="text" name="" value=""></p>
+		<p>請假時間：<input type="text" name="" value=""> to <input type="text" name="" value=""></p>
+		<p>請假時數：<input type="text" name="" value=""></p>
+		<p>假勤項目：<input type="text" name="" value=""></p>
+		<p>職務代理人：<input type="text" name="" value=""></p>
+		<p>簽核代理人：<input type="text" name="" value=""></p>
+		<p>請假原因：<input type="text" name="" value=""></p>
+		<input type="submit" value="Submit">
+		</form>
+	</div>
+
+</div>
+</body>
+</html>
