8c398cf13882ec6235c6f87d9a5c8ccdef45f7cf
David <davidhsieh7988@gmail.com>
2017-09-04 17:22:49 +0800

    delete_leave.php require conn.php

--- HR_ERP/Controller/delete_leave.php
+++ HR_ERP/Controller/delete_leave.php
@@ -1,21 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "peopleresource";
-
-try {
-    $conn = mysqli_connect($servername,$username ,$password,$dbname);
-
+    
+    require 'conn.php';
     // sql to delete a record
     $sql = "DELETE FROM `leave` WHERE id='" . $_GET["id"] . "'";
 
     mysqli_query($conn,$sql);
     header("Location:admin_leave.php");
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
 
 ?>
